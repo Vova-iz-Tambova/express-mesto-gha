12df31035b4bcd41b52d4e72df1698f01469f449
Vova iz Tambova <voller.ruden@yandex.ru>
2023-09-17 20:25:51 +0300

    Реализуйте временное решение авторизации

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6507355882a2eea70241f847'
+  };
+  next();
+});
+
 app.use(express.json());
 app.use('/users', require('./routes/users'));
 
