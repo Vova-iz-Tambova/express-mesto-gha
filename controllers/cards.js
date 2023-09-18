636c41de4783021805c56ab4b5926e2d6ee5bf7b
Vova iz Tambova <voller.ruden@yandex.ru>
2023-09-18 12:59:38 +0300

    Обработка ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,29 +3,79 @@ const Card = require('../models/card')
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then(cards => res.send(cards))
+    .catch((err) => res.status(500)
+      .send({ message: err.message }))
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then(cards => res.send(cards))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400)
+          .send({ message: 'Переданы некорректные данные при создании карточки' });
+      } else {
+        res.status(500)
+          .send({ message: err.message });
+      }
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then(cards => res.send(cards))
+    .then((card) => {
+      if (card) {
+        res.send(card)
+      } else {
+        res.status(404)
+          .send({ message: 'Карточка с указанным _id не найдена' });
+      }
+    })
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true })
-    .then(cards => res.send(cards))
+    .then(card => {
+      if (card) {
+        res.send(card)
+      } else {
+        res.status(404)
+          .send({ message: 'Карточка с указанным _id не найдена' })
+      }
+    })
+    .catch(err => {
+      if (err.name === 'CastError') {
+        res.status(400)
+          .send({ message: 'Переданы некорректные данные для постановки лайка' })
+      } else {
+        res.status(500)
+          .send({ message: err.message })
+      }
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true })
-    .then(cards => res.send(cards))
+    .then(card => {
+      if (card) {
+        res.send(card)
+      } else {
+        res.status(404)
+          .send({ message: 'Карточка с указанным _id не найдена' })
+      }
+    })
+    .catch(err => {
+      if (err.name === 'CastError') {
+        res.status(400)
+          .send({ message: 'Переданы некорректные данные для снятии лайка' })
+      } else {
+        res.status(500)
+          .send({ message: err.message })
+      }
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -3,27 +3,79 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then(users => res.send(users))
+    .catch((err) => res.status(500)
+      .send({ message: err.message }))
 };
 
 module.exports.getUser = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then(user => res.send(user))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404)
+          .send({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.status(500)
+          .send({ message: err.message });
+      }
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then(user => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400)
+          .send({ message: 'Переданы некорректные данные при создании пользователя' });
+      } else {
+        res.status(500)
+          .send({ message: err.message });
+      }
+    });
 };
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate({ name, about }, req.user._id)
+  User.findByIdAndUpdate(req.user._id, { name, about },
+    { runValidators: true })
     .then(user => res.send(user))
+    .catch((err) => {
+      switch (err.name) {
+        case 'ValidationError':
+          res.status(400)
+            .send({ message: 'Переданы некорректные данные при обновлении профиля' });
+          break
+        case 'CastError':
+          res.status(404)
+            .send({ message: 'Пользователь с указанным _id не найден' });
+          break
+        default:
+          res.status(500)
+            .send({ message: err.message });
+      }
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate({ avatar }, req.user._id)
+  User.findByIdAndUpdate(req.user._id, { avatar },
+    { runValidators: true })
     .then(user => res.send(user))
+    .catch((err) => {
+      switch (err.name) {
+        case 'ValidationError':
+          res.status(400)
+            .send({ message: 'Переданы некорректные данные при обновлении аватара' });
+          break
+        case 'CastError':
+          res.status(404)
+            .send({ message: 'Пользователь с указанным _id не найден' });
+          break
+        default:
+          res.status(500)
+            .send({ message: err.message });
+      }
+    });
 };
