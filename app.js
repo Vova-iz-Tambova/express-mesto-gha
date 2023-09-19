0b9541e2897c0555585d0fd4ba01f637cef41f89
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-09-19 12:58:17 +0300

    Обработка неправильного пути 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const NotFound = require('./errors/NotFound')
+const NotFound = require('./errors/NotFound');
 
 const app = express();
 app.use(express.json());
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
 app.use('*', (req, res, next) => {
   next(new NotFound('Страница не найдена'));
 });
