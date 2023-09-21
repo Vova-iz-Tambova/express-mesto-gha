ca0d86494bec3a804f953367589afbdadd6df03f
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-09-21 21:42:43 +0300

    Удалите хардкод

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const auth = require('./middlewares/auth');
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use((req, res, next) => {
-  req.user = {
-    _id: '650c582d3fc56216b89e8ddc',
-  };
-  next();
-});
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
