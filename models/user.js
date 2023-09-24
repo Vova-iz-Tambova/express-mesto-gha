6c36bd6014276e7613b18cfd6c856a4ffe1829e3
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-09-24 10:04:21 +0300

    Сделайте так, чтобы API не возвращал хеш пароля

--- models/user.js
+++ models/user.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Поле "password" должно быть заполнено'],
+    select: false,
   },
 }, { versionKey: false });
 
