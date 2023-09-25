fe69b0365bbf5d0263f7e5cc4c7b899d5b489c79
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-09-25 20:22:46 +0300

    Валидируйте приходящие на сервер запросы

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const { errors } = require('celebrate');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
@@ -22,6 +36,8 @@ app.use('/*', (req, res) => {
   res.status(404)
     .send({ message: 'Страница не найдена' });
 });
+
+app.use(errors());
 
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
@@ -34,7 +50,8 @@ app.use((err, req, res, next) => {
       message: status === 500
         ? 'На сервере произошла ошибка'
         : message,
-    });
+    })
+    .catch(next);
 });
 
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,30 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUser, updateUser, updateAvatar, getCurrentUser,
 } = require('../controllers/users');
 
 router.get('/me', getCurrentUser);
+
 router.get('/', getUsers);
-router.get('/:userId', getUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().alphanum().length(24),
+  }),
+}), getUser);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+}), updateAvatar);
 
 module.exports = router;
